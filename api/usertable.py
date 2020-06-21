16224aca1de799bf2cdac9bdeecb96511a31f1cc
Kogoon <gojs712@gmail.com>
2020-06-21 15:47:09 +0900

    *table.py for db sql...

---
+++ api/usertable.py
@@ -0,0 +1,74 @@
+from database import Database
+import json
+import utils
+
+
+#
+#
+#
+class UserTable(Database):
+    
+    #
+    def get(self, user_login):
+        
+        sql =  "SELECT ID, user_login, user_pass, "
+        sql += "user_nicename, user_email, display_name FROM wp_users "
+        sql += "WHERE user_login={};".format(user_login)
+
+        print("DEBUG SQL ===> {}".format(sql))
+        
+        result = ()
+        try:
+            self.cursor.execute(sql)
+            result = self.cursor.fetchall()
+        except:
+            return {"error":"{}".format(e)}
+        
+        result = {} if len(result) == 0 else result[0]
+
+        return result
+
+    #
+    def list(self, page=0, itemsInPage=20):
+        
+        page = page * itemsInPage # 얼마나 많은 데이터를 볼 것인가. 한 page에.
+        # 많은 정보를 보게 되니 비슷한 값은 줄여서 표시
+        sql =  "SELECT ID, user_login, user_pass, user_email, user_registered, display_name "
+        sql += "FROM wp_users "
+        sql += "LIMIT {p}, {IIP}".format(p=page, IIP=itemsInPage)
+
+        print("DEBUG SQL ===> {}".format(sql))
+
+        self.cursor.execute(sql)
+        result = self.cursor.fetchall()
+
+        return result
+
+    #
+    def insert(self, j):
+        pass
+
+    #
+    def update(self, user_login, j):
+        pass
+
+    #
+    #
+    def delete(self, user_login):
+        
+        sql =  "DELETE FROM wp_users "
+        sql += "WHERE user_login='{}'".format(id)
+
+        print("DEBUG SQL ===> {}".format(sql))
+        
+        result = None
+        try:
+            self.cursor.execute(sql)
+            self.db.commit()
+        except Exception as e:
+            result = {"error":"{}".format(e)}
+
+        return result
+
+
+
