e67cbf11bde936174c1c02a05692670b58647c70
Kogoon <gojs712@gmail.com>
2020-06-23 22:44:07 +0900

    modified algorithm

--- api/algorithm/q1.py
+++ api/algorithm/q1.py
@@ -1,7 +1,8 @@
 """
-INPUT : N(int) (0 < r <= N)
-OUTPUT : list -> Ascending
+INPUT  > N(int) ( 0 < r <= N)
+OUTPUT > gen_list, sorted(Ascending)_list
 """
+
 import random
 import sys
 import time
@@ -19,31 +20,26 @@ def gen_random(N):
 
 
 # sorted algorithm.
-def sorted_random(A):
-
-    MAX_NUM = 1000
-    N = len(A)
-    count = [0] * (MAX_NUM+1) #초기화
-    countSum = [0] * (MAX_NUM+1)
-
-    for i in range(0, N):
-        count[A[i]] += 1
-
-    countSum[0] = count[0]
-    for i in range(1, MAX_NUM+1):
-        countSum[i] = countSum[i-1] + count[i]
-
-    B = [0] * (N+1)
-
-    for i in range(N-1, -1, -1):
-        B[countSum[A[i]]] = A[i]
-        countSum[A[i]] -= 1
-
-    result = ""
-    for i in range(1, N+1):
-        result += str(B[i]) + " "
+def sorted_random(sorting, left, right):
     
-    return result
+    if right <= left:
+        return
+    a = i = left
+    b = right
+    pivot = sorting[left]
+    while i <= b:
+        if sorting[i] < pivot:
+            sorting[a], sorting[i] = sorting[i], sorting[a]
+            a += 1
+            i += 1
+        elif sorting[i] > pivot:
+            sorting[b], sorting[i] = sorting[i], sorting[b]
+            b -= 1
+        else:
+            i += 1
+    sorted_random(sorting, left, a - 1)
+    sorted_random(sorting, b + 1, right)
+
 
 
 if __name__=="__main__":
@@ -51,4 +47,5 @@ if __name__=="__main__":
     N = int(input())
     random_gen = gen_random(N)
     print(random_gen)
-    print(sorted_random(random_gen))
+    sorted_random(random_gen, 0, len(random_gen)-1)
+    print(random_gen)
